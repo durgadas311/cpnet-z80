c123493df54e74a332a35b647b061801b99fb028
DavidJRichards <richards.malton@gmail.com>
2020-12-11 23:20:19 +0000

    Save work in progress before refactoring file structure

---
+++ contrib/serialserver.bat
@@ -0,0 +1,2 @@
+rem serialserver
+java -cp CpnetSerialServer.jar;jSerialComm-2.6.2.jar CpnetSerialServer
---
+++ contrib/socketserver.bat
@@ -0,0 +1,2 @@
+rem # Wrapper script to startup CpnetSocketServer on Windows
+java -cp CpnetSocketServer.jar CpnetSocketServer
