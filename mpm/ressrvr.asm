af6b4b10239994740e4eb52c1ff2256008ee1564
Douglas Miller <durgadas311@gmail.com>
2021-10-31 08:07:55 -0500

    Use conventional process descriptor layout in RESSRVR.ASM

--- mpm/ressrvr.asm
+++ mpm/ressrvr.asm
@@ -16,14 +16,34 @@
 bdose:	dw	0
 ; Main process descriptor - "SERVR0PR"
 SERVR0PR:
-	dw	0	; link
-	db	0	; status
-	db	100	; priority
-	dw	$-$	; initial SP set from BNKSRVR.BRS
-	; extended errors and no abort options
-	db	'SERV','R'+80h,'0P','R'+80h
-	db	0,0ffh,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	dw	0		; link
+	db	0		; status
+	db	100		; priority
+	dw	0		; stack pointer - in banked portion
+	db	'SERV','R'+80h,'0P','R'+80h ; name "SERVR0PR" with
+				; "return errors" (N4') and
+				; "no abort" (N7') options.
+	db	0		; console
+	db	0		; memseg - banked portion exists...
+	ds	2		; b
+	ds	2		; thread
+	ds	2		; buff
+	ds	1		; user code & disk slct
+	ds	2		; dcnt
+	ds	1		; searchl
+	ds	2		; searcha
+	ds	2		; active drives
+	dw	0		; HL'
+	dw	0		; DE'
+	dw	0		; BC'
+	dw	0		; AF'
+	dw	0		; IY
+	dw	0		; IX
+	dw	0		; HL
+	dw	0		; DE
+	dw	0		; BC
+	dw	0		; AF, A = ntwkif console dev #
+	ds	2		; scratch
 
 	; the additional processes...
 	ds	(nmb$rqstrs-1)*P$LEN
